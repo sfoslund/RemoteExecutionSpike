111b239ce6295f3fc2a2a3cb9c184ea164704b82
sfoslund <sfoslund@microsoft.com>
2022-12-28 11:02:21 -0800

    Investigating execute

--- RemoteExecutionSpike/Program.cs
+++ RemoteExecutionSpike/Program.cs
@@ -2,48 +2,35 @@
 using Interop.UIAutomationClient;
 using Windows.UI.UIAutomation.Core;
 using Windows.UI.UIAutomation;
-
+using System.Runtime.InteropServices;
+using System;
 
 namespace RemoteExecutionSpike
 {
     internal class Program
     {
-        private static int instructionId = 0;
-
         static void Main(string[] args)
         {
             RemoteExecution();
         }
 
-        // TODO example: https://github.com/microsoft/Microsoft-UI-UIAutomation/blob/f36cdb687fae4a397fc39f941ea5a7281211f3cf/src/UIAutomation/Microsoft.UI.UIAutomation/AutomationRemoteOperation.cpp#L302
         private static void RemoteExecution()
         {
-            // TODO how to specify pid? https://github.com/microsoft/Microsoft-UI-UIAutomation/blob/f36cdb687fae4a397fc39f941ea5a7281211f3cf/src/UIAutomation/FunctionalTests/WinRTBuilderTests.cpp
             CoreAutomationRemoteOperation coreOp = new CoreAutomationRemoteOperation();
-            ImportElement(coreOp);
+
             var instructions = GetInstructions();
-            var executionResult = coreOp.Execute(instructions); // performs a blocking cross-process call
+            var pinndeObj = GCHandle.Alloc(instructions, GCHandleType.Pinned);
+            IntPtr dataPointer = (nint)pinndeObj;
+
+            // Unhandled exception. System.Runtime.InteropServices.COMException (0x8000FFFF): Catastrophic failure (0x8000FFFF (E_UNEXPECTED))
+            var executionResult = coreOp.Execute(instructions);//dataPointer as object as byte[]);
             Console.WriteLine("Execution status: " + executionResult.Status);
-        }
-
-        private static void ImportElement(CoreAutomationRemoteOperation coreOp)
-        {
-            IUIAutomation uia = new CUIAutomation8();
-            var focusedElement = uia.GetFocusedElement();
-            Console.WriteLine("Focused element: " + focusedElement.CurrentName);
-
-            var element = focusedElement as object as AutomationElement;
-            var elementId = new AutomationRemoteOperationOperandId(instructionId);
-            instructionId++;
-
-            Console.WriteLine("Importing element");
-            coreOp.ImportElement(elementId, element); // TODO throws-> accessing protected memory
-            Console.WriteLine("Import element finished");
         }
 
         private static byte[] GetInstructions()
         {
-            return new byte[1024]; // TODO
+            byte[] arr = new byte[] { 0x00, 0x00, 0x00, 0x00, 0x4c, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x4d, 0x00, 0x00, 0x00, 0x03, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00 };
+            return arr;
         }
     }
 }
